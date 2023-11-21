a2dd0a30bbe6a7ad33133b1ec4e92e9a99b1427d
lauro-sa <108936697+lauro-sa@users.noreply.github.com>
2023-11-21 16:57:25 -0300

    Cambios 0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'; // Creo que en versiones recientes no es necesario
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Importando el componente App
+import './index.css'; // Importando estilos globales
 
+// Colocando la aplicación en el elemento raíz del DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
